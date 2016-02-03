df4ffc51d6f74567ca22d271d5b0e43b437b5046
jammat <jammat@utu.fi>
2016-02-03 16:04:36 +0200

    Update Tehdas.java

--- src/server/Tehdas.java
+++ src/server/Tehdas.java
@@ -7,16 +7,16 @@ import java.rmi.server.*;
 	public class Tehdas extends UnicastRemoteObject implements tehtaanOlemus {
 
 		private siilo[] siilot;
-		private ruuviKuljetin[] ruuvikuljettimet;
-		private pummpu[] pumput;
+		private ruuvikuljetin[] ruuvikuljettimet;
+		private pumppu[] pumput;
 		private prosessori[] prosessorit;
 		private sailio[] kypsytyssailiot; 
 		
 		
 		public Tehdas() throws RemoteException {
 			super();
-			siilot = new Siilo[4]; 								//4 siiloa
-			ruuvikuljettimet = new ruuviKuljetin[3]; 			//3 kuljetinta
+			siilot = new siilo[4]; 								//4 siiloa
+			ruuvikuljettimet = new ruuvikuljetin[3]; 			//3 kuljetinta
 			pumput = new pumppu[4]; 							//4 pumppua
 			prosessorit = new prosessori[3];					//3 prosessoria
 			kypsytyssailiot	  = new sailio[10]; 				//10 kypsytyssäiliötä
