ef1abcffc2533cdda0fe9cea31e89965bb5f86fd
jammat <jammat@utu.fi>
2016-02-17 17:33:37 +0200

    Update sailio.java

--- src/server/sailio.java
+++ src/server/sailio.java
@@ -1,5 +1,7 @@
 package server;
 
+import hojserver.KoneenTila;
+
 public class sailio extends Thread{
 
 	private int tilavuus = 10000;
@@ -16,4 +18,72 @@ public class sailio extends Thread{
 		pump=-1;
 		bottlepump=-1;
 	}
+	public void setTila(KoneenTila t){
+		if(t == KoneenTila.FREE){
+			setPump(-1);
+			setBottlePump(-1);
+		}
+		tila = t;
+	}
 	
+	public KoneenTila getTila(){
+		return tila;
+	}
+	
+	public synchronized boolean isReserved(){
+		return reserved;
+	}
+	
+	public synchronized void setReserved(boolean r){
+		if(r == false){
+			setBottlePump(-1);
+			if(isFull()){
+				setTila(KoneenTila.FULL);
+			} else {
+				setTila(KoneenTila.FREE);
+			}
+		}
+		reserved = r;
+	}
+	
+	public boolean isFull(){
+		if (amountOfLiquid >= tilavuus){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public int getPump(){
+		return pump;
+	}
+	
+	public void setPump(int p){
+		pump = p;
+	}
+	
+	public int getBottlePump(){
+		return bottlepump;
+	}
+	
+	public void setBottlePump(int p){
+		bottlepump = p;
+	}
+	
+	public synchronized int getAmountOfLiquid(){
+		return amountOfLiquid;
+	}
+	
+	public synchronized void takeLiquid(int amount){
+		if(amount <= amountOfLiquid){
+			amountOfLiquid -= amount;
+		}
+	}
+	
+	public synchronized void addLiquid(int amount){
+		if(amount <=  tilavuus - amountOfLiquid){
+			amountOfLiquid += amount;
+		}
+	}
+	
+}
